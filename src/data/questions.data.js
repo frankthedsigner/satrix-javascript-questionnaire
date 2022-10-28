bd781c3e7c9620e0c49623f3f0bcf9d77392890d
frankthedsigner <19155655+frankthedsigner@users.noreply.github.com>
2022-10-28 16:22:32 +0200

    v.0.0.1 - Setup and styling
    Setup project structure with styling.

---
+++ src/data/questions.data.js
@@ -0,0 +1,132 @@
+export const questions = [
+  {
+    question1: {
+      question: {
+        value:
+          'If you want access to companies in developed markets across the world, you need…',
+      },
+      answers: {
+        answer1: { state: false, value: 'local equity' },
+        answer2: { state: true, value: 'global equity' },
+        answer3: { state: false, value: 'local money market exposure' },
+      },
+    },
+  },
+  {
+    question2: {
+      question: {
+        value: 'What portion of Satrix’s staff are women?',
+      },
+      answers: {
+        answer1: { state: false, value: 'About 30%' },
+        answer2: { state: true, value: 'About 50%' },
+        answer3: { state: false, value: 'All of them' },
+      },
+    },
+  },
+  {
+    question3: {
+      question: {
+        value:
+          'To invest in companies in developed markets across the world, your best option is…',
+      },
+      answers: {
+        answer1: { state: false, value: 'Satrix Top 40 ETF' },
+        answer2: { state: true, value: 'Satrix MSCI World ETF' },
+        answer3: { state: false, value: 'Satrix Money Market Fund' },
+      },
+    },
+  },
+  {
+    question4: {
+      question: {
+        value: 'What does ESG stand for?',
+      },
+      answers: {
+        answer1: { state: true, value: 'Environmental, social & governance' },
+        answer2: { state: false, value: 'Economic, sectoral & governmental' },
+        answer3: { state: false, value: 'Equities, shares & growth' },
+      },
+    },
+  },
+  {
+    question5: {
+      question: {
+        value: 'What is the JSE’s annual Women’s Month event called?',
+      },
+      answers: {
+        answer1: { state: false, value: 'JSE 4 Women' },
+        answer2: { state: false, value: 'GirlStocks' },
+        answer3: { state: true, value: 'JSE She Invests' },
+      },
+    },
+  },
+  {
+    question6: {
+      question: {
+        value: 'What is South Africa’s contribution to global GDP?',
+      },
+      answers: {
+        answer1: { state: true, value: 'About 0.5%' },
+        answer2: { state: false, value: 'About 3%' },
+        answer3: { state: false, value: 'About 5%' },
+      },
+    },
+  },
+  {
+    question7: {
+      question: {
+        value: 'How frequently is the Satrix Momentum index rebalanced?',
+      },
+      answers: {
+        answer1: { state: true, value: '8 times a year' },
+        answer2: { state: false, value: 'Once a quarter' },
+        answer3: { state: false, value: 'Once a year' },
+      },
+    },
+  },
+  {
+    question8: {
+      question: {
+        value: 'The Satrix INDI tracks an index of…',
+      },
+      answers: {
+        answer1: {
+          state: false,
+          value:
+            'the 25 largest industrial companies, ranked by investable market cap',
+        },
+        answer2: {
+          state: true,
+          value:
+            'the top 40 companies on the JSE, ranked by investable market cap',
+        },
+        answer3: { state: false, value: 'listed property companies' },
+      },
+    },
+  },
+  {
+    question9: {
+      question: {
+        value: 'How are companies chosen for the Top 40 Index?',
+      },
+      answers: {
+        answer1: { state: false, value: 'By number of employees' },
+        answer2: { state: true, value: 'By investable market cap' },
+        answer3: { state: false, value: 'By profit margin' },
+      },
+    },
+  },
+  {
+    question10: {
+      question: {
+        value: 'What is not the same as a share?',
+      },
+      answers: {
+        answer1: { state: false, value: 'Equity' },
+        answer2: { state: true, value: 'Stock' },
+        answer3: { state: false, value: 'Risk' },
+      },
+    },
+  },
+]
